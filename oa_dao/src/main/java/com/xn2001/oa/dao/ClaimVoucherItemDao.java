b6dc738c5bd02aad3e38610d79d615544ba46268
lexinhu <jialna@qq.com>
2020-02-24 22:55:02 +0800

    报销单持久层

--- oa_dao/src/main/java/com/xn2001/oa/dao/ClaimVoucherItemDao.java
+++ oa_dao/src/main/java/com/xn2001/oa/dao/ClaimVoucherItemDao.java
@@ -1,12 +1,14 @@
 package com.xn2001.oa.dao;
 
 import com.xn2001.oa.entity.ClaimVoucherItem;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by 乐心湖 on 2020/2/23 23:56
  */
+@Repository
 public interface ClaimVoucherItemDao {
     void insert(ClaimVoucherItem claimVoucherItem);
     void update(ClaimVoucherItem claimVoucherItem);
--- oa_dao/src/main/java/com/xn2001/oa/dao/DealRecordDao.java
+++ oa_dao/src/main/java/com/xn2001/oa/dao/DealRecordDao.java
@@ -1,12 +1,14 @@
 package com.xn2001.oa.dao;
 
 import com.xn2001.oa.entity.DealRecord;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by 乐心湖 on 2020/2/24 0:00
  */
+@Repository
 public interface DealRecordDao {
     void insert(DealRecord dealRecord);
     List<DealRecord> selectByClaimVoucher(int cvid);
